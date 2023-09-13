6bfa466937b2f380d8e643fe773cdfe42ad85585
hem1t <sunny10fb@gmail.com>
2023-09-13 15:13:15 +0530

    config implimentatioN

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub enum Category {
     General,
     Anime,
     People,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub enum Purity {
     SFW,
     Sketchy,
@@ -24,10 +24,32 @@ pub enum Sorting {
     Toplist,
 }
 
+impl Sorting {
+    pub fn to_string(&self) -> &str {
+        match self {
+            Sorting::DateAdded => "date_added",
+            Sorting::Relevance => "relevance",
+            Sorting::Random => "random",
+            Sorting::Views => "views",
+            Sorting::Favorites => "favorites",
+            Sorting::Toplist => "toplist",
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub enum Order {
     Desc,
     Asc,
+}
+
+impl Order {
+    pub fn to_string(&self) -> &str {
+        match self {
+            Order::Desc => "desc",
+            Order::Asc => "asc",
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -39,6 +61,20 @@ pub enum TopRange {
     ThreeMonth,
     SixMonth,
     OneYear,
+}
+
+impl TopRange {
+    pub fn to_string(&self) -> &str {
+        match self {
+            TopRange::OneDay => "1d",
+            TopRange::ThreeDay => "3d",
+            TopRange::OneWeek => "1w",
+            TopRange::OneMonth => "1M",
+            TopRange::ThreeMonth => "3M",
+            TopRange::SixMonth => "6M",
+            TopRange::OneYear => "1y",
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -74,8 +110,14 @@ pub enum Color {
     H424153,
 }
 
+impl Color {
+    pub fn to_str(&self) -> &str {
+        stringify!(self)
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Query {
+pub struct SearchURL {
     query: String,
     category: Vec<Category>,
     purity: Vec<Purity>,
@@ -85,22 +127,101 @@ pub struct Query {
     atleast: String,
     resolutions: Vec<String>,
     ratios: Vec<String>,
-    colors: Color,
-    page: usize,
+    colors: Vec<Color>,
+    page: u32,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub enum QueryOptions {
-    Search(Box<Query>),
-    Collection(Box<String>),
-    Tag(Box<String>),
+pub enum Query {
+    Search(Box<SearchURL>),
+    Collection {
+        username: Box<String>,
+        id: Box<String>,
+    },
+    ID(Box<String>),
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct AppConfig {
-    apikey: String,
-    query: QueryOptions,
-    seconds: u8,
+    apikey: Option<String>,
+    username: Option<String>,
+    query: Query,
+    seconds: Option<u32>,
     // Assuming Scripts takes a pathName to wall.
     script: String,
+}
+
+impl Query {
+    pub fn tourl(self) -> String {
+        match self {
+            Query::ID(id) => format!("https://wallhaven.cc/api/v1/w/{}", id),
+            Query::Search(search) => {
+                format!("https://wallhaven.cc/api/v1/search?{}", search.tourl())
+            }
+            Query::Collection { username, id } => format!(
+                "https://wallhaven.cc/api/v1/collections/{}/{}",
+                &username, &id
+            ),
+        }
+    }
+}
+
+impl SearchURL {
+    pub fn tourl(self) -> String {
+        format!("q={}&categories={}&purity={}&sorting={}&order={}&topRange={}&atleast={}&resolutions={}&ratios={}&colors={}&page={}",
+                self.query,
+                self.category_str(),
+                self.purity_str(),
+                self.sort.to_string(),
+                self.order.to_string(),
+                self.top_range.to_string(),
+                self.atleast,
+                self.resolutions_str(),
+                self.ratios_str(),
+                self.color_str(),
+                self.page
+        )
+    }
+
+    pub fn category_str(&self) -> String {
+        let mut s = String::with_capacity(3);
+        let category = |c: Category| {
+            if self.category.contains(&c) {
+                '1'
+            } else {
+                '0'
+            }
+        };
+        s.push(category(Category::General));
+        s.push(category(Category::Anime));
+        s.push(category(Category::People));
+        s
+    }
+
+    pub fn purity_str(&self) -> String {
+        let mut s = String::with_capacity(3);
+        let purity = |c: Purity| {
+            if self.purity.contains(&c) {
+                '1'
+            } else {
+                '0'
+            }
+        };
+        s.push(purity(Purity::SFW));
+        s.push(purity(Purity::Sketchy));
+        s.push(purity(Purity::NSFW));
+        s
+    }
+
+    pub fn resolutions_str(&self) -> String {
+        self.resolutions.join(",")
+    }
+
+    pub fn ratios_str(&self) -> String {
+        self.ratios.join(",")
+    }
+
+    pub fn color_str(&self) -> String {
+        self.colors.iter().map(|c| c.to_str()).collect::<String>()
+    }
 }
